fbba96fecc8730e0a041c4bf08d67df086448438
PlinioMoraes <psmoraes@inf.ufpel.edu.br>
2023-06-12 21:36:06 -0300

    Fix: Teste 2, added validations

--- teste2.js
+++ teste2.js
@@ -1,17 +1,27 @@
 var data =  require("./Teste1/database/fakeData");
 
 module.exports = function(req, res){
-  
-    var name =  req.body.name;
-    var job =  req.body.job;
+    const { name, job } = req.body;
     
-    var newUser = {
+    if (name === undefined || job === undefined || name === "" || job === "") {
+        return res.status(400).send({ message: "Invalid data"});
+    }
+
+    let newUser = {
         name: name,
         job: job,
+    }
+    
+    const getUser = data.find(user => user.name === name);
+    if (getUser) {
+        return res.status(409).send({ message: "User already exists"});
+    }
+
+    if (data.includes(newUser)) {
+        return res.status(409).send({ message: "User already exists"});
     }
 
     data.push(newUser)
     
-    res.send(newUser);
-
+    return res.status(200).send({ message: "User created successfully"});
 };
